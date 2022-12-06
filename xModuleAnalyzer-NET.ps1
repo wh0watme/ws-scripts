5384e9896e8b6663bf80451a90aee9015226d1a8
wh0watme <ghost565269@gmail.com>
2022-12-06 15:35:35 -0500

    Update xModuleAnalyzer-NET.ps1
    Changes

--- xModuleAnalyzer-NET.ps1
+++ xModuleAnalyzer-NET.ps1
@@ -1,9 +1,5 @@
 Set-Variable SEARCHDIR (Get-Location)
 
-if (Test-Path -Path .\multi_module.ps1 -PathType Leaf) {
-   Remove-Item .\multi_module.ps1
-}
-
 Get-ChildItem -Include "*.csproj" -Recurse | Where-Object DirectoryName -NotLike *build* | Where-Object DirectoryName -NotLike *test* | Where-Object DirectoryName -NotLike *host* | Where-Object DirectoryName -NotLike *migration*  | ForEach-Object {
      Set-Variable CSPROJDIR (Resolve-Path $_ | Split-Path -parent)
      Write-Output "CSPROJDIR= $CSPROJDIR"
@@ -14,6 +10,6 @@ Get-ChildItem -Include "*.csproj" -Recurse | Where-Object DirectoryName -NotLike
         Write-Output "DLL= $DLL"
         Set-Variable RELCSPROJDIR (Resolve-Path -Relative $CSPROJDIR)
         Write-Output "RELCSPROJDIR= $RELCSPROJDIR"
-        Write-Output "java -XX:+UseG1GC -Xmx8g -jar $JARFILE -appPath $DLL -d $RELCSPROJDIR -project $CSPROJ" >>multi_module.ps1
+        java -XX:+UseG1GC -Xmx8g -jar $JARFILE -appPath $DLL -d $RELCSPROJDIR -project $CSPROJ 
      }
    }
